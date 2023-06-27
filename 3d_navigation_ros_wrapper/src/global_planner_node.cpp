c7bdc78293c074daace44a17bd2bfbff11ecf35e
Kirill Lalayants <lalayantskirill@gmail.com>
2023-06-27 17:43:58 +0300

    no octomap,  succesful build

--- 3d_navigation_ros_wrapper/src/global_planner_node.cpp
+++ 3d_navigation_ros_wrapper/src/global_planner_node.cpp
@@ -2,13 +2,13 @@
 
 int main(int _argc, char **_argv){
     srand((unsigned int) time(0));
-    ros::init(argc, argv, "global_planner");
+    ros::init(_argc, _argv, "global_planner");
     ros::NodeHandle node_handler;
     ros::Rate rate(10);
 
-    GlobalPlanner3dNodeWrapper 3dplanner_wrapper(ros::NodeHandle & _node_handler);
+    GlobalPlanner3dNodeWrapper wrapper(ros::NodeHandle & _node_handler);
     while(ros::ok){
-        std::cout << '1\n' << std::endl;
+        std::cout << "1\n" << std::endl;
         ros::spinOnce();
         rate.sleep();
     }
